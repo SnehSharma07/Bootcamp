8138f24f80e8c2756600876589554a1e529b7806
Sneh Sharma <snehsharma415@gmail.com>
2026-06-16 13:28:03 +0530

    Update

---
+++ vite project/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Home from './Home';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <div>App</div>
+
+      <h1>{count}</h1>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+
+      <Home />
+    </>
+  );
+};
+
+export default App;
